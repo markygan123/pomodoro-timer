864fb6f7cc8084b25d90a8031f7d770c25ed1dfb
markygan123 <macawilemarkygan@gmail.com>
2020-08-05 03:03:30 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const minutesEl = document.querySelector(".timer__minutes");
+const secondsEl = document.querySelector(".timer__seconds");
+const timerToggleEl = document.getElementById("timer__toggle");
+const startingMinutes = 25;
+let countDownTime = startingMinutes * 60;
+let timer = null;
+
+
+minutesEl.textContent = startingMinutes < 10 ? "0" + startingMinutes : startingMinutes;
+secondsEl.textContent = "00";
+
+
+
+function startTimer() {
+    let minutes = Math.floor(countDownTime / 60);
+    let seconds = countDownTime % 60;
+
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    if (minutes === "00" && seconds === "00") {
+        setTimeout(() => {
+            window.location.reload(); 
+        }, 1000);
+    }
+
+    minutesEl.textContent = minutes;
+    secondsEl.textContent = seconds;
+
+    countDownTime !== 0 ? countDownTime-- : countDownTime;
+}
+
+function main() {
+    timerToggleEl.addEventListener("click", function () {
+        if (timer !== null) {
+            clearInterval(timer);
+            timer = null;
+        } else {
+            timer = setInterval(startTimer, 1000);
+        }
+
+        timerToggleEl.innerHTML === "start" ?
+        timerToggleEl.innerHTML = "stop":
+        timerToggleEl.innerHTML = "start";
+    });
+}
+
+main();
